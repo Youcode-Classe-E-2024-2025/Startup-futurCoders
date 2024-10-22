a04b5fd72054dee400943a4489206c4413455ac5
Khawla Boukniter <boukniter.khawla@gmail.com>
2024-10-22 20:04:22 +0100

    Ajouter l'interaction du menu burger avec le menu de navigation

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,8 @@
 console.log("hello!");
+
+mynav = document.getElementById("sidenav");
+openbtn = document.getElementById("burger-icon");
+
+openbtn.addEventListener("click", function () {
+  mynav.classList.toggle("show");
+});
